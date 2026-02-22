d7411e573e2572145f7e297f3660de2c36301946
krishna-github-59 <krishnasai0509@gmail.com>
2026-02-22 12:23:14 +0530

    URL path correction in auth

--- Backend/src/main/java/com/club/events_dashboard/controller/AuthController.java
+++ Backend/src/main/java/com/club/events_dashboard/controller/AuthController.java
@@ -11,7 +11,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
 
     @Autowired
